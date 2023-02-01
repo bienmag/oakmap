b77d4da34e951ca9b5276f39865357a41b0ce0f5
bienmag <mvaretckaia@gmail.com>
2023-02-01 17:22:30 +0100

    fix: styles markdown renders correctly

--- apps/web/tailwind.config.js
+++ apps/web/tailwind.config.js
@@ -9,6 +9,9 @@ module.exports = {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
+    fontFamily: {
+      'sans': ['ui-sans-serif', 'system-ui'],
+    },
     extend: {},
   },
   plugins: [],

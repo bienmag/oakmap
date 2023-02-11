101fefb2299faf4ba21bad931da209b67071dd4b
Margarita Varetckaia <mvaretckaia@gmail.com>
2023-02-11 20:38:44 +0100

    feat: link/unlink (not finished)

--- apps/server/src/Models/Leaves.ts
+++ apps/server/src/Models/Leaves.ts
@@ -1,5 +1,5 @@
-import { DBMarkdown, DBTree } from "../lib/mongo";
-import { ObjectId } from "mongodb";
+import { DBTree } from "../lib/mongo";
+const mongodb = require('mongodb');
 
 class Leaf {
   constructor(
@@ -26,9 +26,67 @@ class Leaf {
     leafName?: string,
     branchId?: string
   ): Promise<Leaf> {
+
+    let update: { position?: object, leafName?: string, branchId?: string } = {}
+
+    if (position) update['position'] = position;
+    if (leafName) update['leafName'] = leafName;
+    if (branchId) update['branchId'] = branchId
+
+    const id = new mongodb.ObjectId(treeId)
+
+
+
+    // let tree = await DBTree.findById({ _id: id, "unlinkedLeaves.leafId": leafId })
+    // if (!tree) {
+    //   throw new Error("There is no leaf with this leafId in the tree")
+
+
+
+    const tree = await DBTree.findById({ _id: id, branches: { $elemMatch: { branchId: branchId, leaves: { $elemMatch: { leafId: leafId } } } } })
+
+
+    console.log(tree)
+    // WORKING TO UPDAATE THE LEaF IN UNLINKED LEAVES!!!!!!!!!!!! can try to use this  { $set: update },
+    // const tree = await DBTree.findOneAndUpdate(
+    //   {
+    //     _id: id, "unlinkedLeaves.leafId": leafId
+    //   }, {
+    //   $set: {
+    //     "unlinkedLeaves.$.position": position,
+    //     "unlinkedLeaves.$.leafName": leafName,
+    //     "unlinkedLeaves.$.branchId": branchId
+    //   }
+    // },
+    //   { new: true })
+
+
+
+
+
+
+
+
+
+
     return new Leaf(leafId, treeId, position, leafName, branchId)
   }
 
+
+  static async linkUnlink(
+    treeId: string,
+    branchId: string,
+    leafId: string) {
+
+    if (branchId === null) {
+      // i have to unlink the leaf and push it to unlinkedLeaves
+    } else {
+
+      // link leaf to the branch
+    }
+
+
+  }
 
 
   static async deleteLeaf(leafId: string) {

49a97926d8176f1d24df3df854c4158b39b37726
Josh <87762857+radna0@users.noreply.github.com>
2023-02-13 13:22:15 +0700

    adding template test

--- apps/server/test/index.test.js
+++ apps/server/test/index.test.js
@@ -1,28 +1,75 @@
-require('jest')
-const fs = require('fs')
-const path = require('path')
-const EasyGraphQLTester = require('easygraphql-tester')
+const { gql, ApolloClient, InMemoryCache } = require('@apollo/client')
 
-const schemaCode = fs.readFileSync(
-  path.join(__dirname, '.', 'schema.graphql'),
-  'utf-8'
-)
-describe('Queries', () => {
+describe('GraphQL', () => {
   let tester
+
   beforeAll(() => {
-    tester = new EasyGraphQLTester(schemaCode)
+    tester = new ApolloClient({
+      uri: 'https://spacex-production.up.railway.app/',
+      cache: new InMemoryCache(),
+    })
   })
 
-  test('Should validate that the server is healthy', () => {
-    const query = `#graphql
-	    query Query {
-            getTrees() {
-                _id
-                treeName
-                description
+  describe('Schemas', () => {
+    it('Should Validate Tree Structure', () => {
+      expect(sum(1, 2)).toBe(1)
+    })
+  })
+
+  describe('Queries', () => {
+    it('Should get CEO of SpaceX', async () => {
+      const res = await tester.query({
+        query: gql`
+          {
+            company {
+              ceo
             }
-        }
-    `
-    tester.test(true, query)
+          }
+        `,
+      })
+      expect(!!res.data.errors).toBe(false)
+      expect(!!res.data.company.ceo).toBe(true)
+    })
+    it('Should get 3 Cores', async () => {
+      const res = await tester.query({
+        query: gql`
+          query Launchpads($limit: Int) {
+            cores(limit: $limit) {
+              id
+              status
+              block
+            }
+          }
+        `,
+        variables: {
+          limit: 3,
+        },
+      })
+      expect(!!res.data.errors).toBe(false)
+      expect(res.data.cores.length).toBe(3)
+    })
+
+    test('Should Get All Trees', () => {
+      expect(sum(1, 2)).toBe(1)
+    })
+    test('Should Get A Specific Tree', () => {
+      expect(sum(1, 2)).toBe(1)
+    })
+  })
+
+  function sum(a, b) {
+    return a + b
+  }
+
+  describe('Mutations', () => {
+    it('Should Create A Tree', () => {
+      expect(sum(1, 2)).toBe(1)
+    })
+    it('Should Update A Tree', () => {
+      expect(sum(1, 2)).toBe(1)
+    })
+    it('Should Delete A Tree', () => {
+      expect(sum(1, 2)).toBe(1)
+    })
   })
 })

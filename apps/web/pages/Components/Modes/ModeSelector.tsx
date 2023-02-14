caaf7723d95b0095024f2d0d41c9ecc75cbec87f
cdev17 <corycholm@gmail.com>
2023-02-14 17:46:46 +0100

    Click blue plus button to create a tree and switch to Tree Editor Mode

---
+++ apps/web/pages/Components/Modes/ModeSelector.tsx
@@ -0,0 +1,23 @@
+import React, {
+    useState,
+  } from 'react'
+  import 'reactflow/dist/style.css'
+import { TreeEditorMode } from "./TreeEditorMode"
+import DashboardMode from "./DashboardMode"
+
+// dashboardMode: any,treeEditorMode: any
+function ModeSelector({ treeMode, setTreeMode, marked, setMarked, renderPage, setRenderPage }: any ){
+ 
+
+  return(
+    <div> {/* Render dashboard by default */ }
+      {renderPage === 'tree' ? <TreeEditorMode treeMode={treeMode} setTreeMode={setTreeMode} marked={marked} setMarked={setMarked}  /> : renderPage === 'profile' ? <div>PROFILE PLACEHOLDER</div> : <DashboardMode renderPage={renderPage} setRenderPage={setRenderPage} />}
+    </div>
+    
+  )
+
+}
+
+
+
+export default ModeSelector

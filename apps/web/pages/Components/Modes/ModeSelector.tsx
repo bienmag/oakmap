0924a23d5b445967857dbac79fb503395372d566
cdev17 <corycholm@gmail.com>
2023-02-14 18:25:17 +0100

    props drilling for nodes and edges from sidebar down

--- apps/web/pages/Components/Modes/ModeSelector.tsx
+++ apps/web/pages/Components/Modes/ModeSelector.tsx
@@ -6,12 +6,36 @@ import { TreeEditorMode } from "./TreeEditorMode"
 import DashboardMode from "./DashboardMode"
 
 // dashboardMode: any,treeEditorMode: any
-function ModeSelector({ treeMode, setTreeMode, marked, setMarked, renderPage, setRenderPage, currentTreeId, setCurrentTreeId }: any ){
+function ModeSelector({ treeMode,
+  setTreeMode,
+  marked,
+  setMarked,
+  renderPage,
+  setRenderPage,
+  currentTreeId,
+  setCurrentTreeId,
+  nodes,
+  setNodes,
+  onNodesChange,
+  setEdges
+}: any) {
  
 
   return(
     <div> {/* Render dashboard by default */ }
-      {renderPage === 'tree' ? <TreeEditorMode treeMode={treeMode} setTreeMode={setTreeMode} currentTreeId={currentTreeId} setCurrentTreeId={setCurrentTreeId} marked={marked} setMarked={setMarked}  /> : renderPage === 'profile' ? <div>PROFILE PLACEHOLDER</div> : <DashboardMode renderPage={renderPage} setRenderPage={setRenderPage} />}
+      {renderPage === 'tree' ? <TreeEditorMode
+        treeMode={treeMode}
+        setTreeMode={setTreeMode}
+        currentTreeId={currentTreeId}
+        setCurrentTreeId={setCurrentTreeId}
+        marked={marked}
+        setMarked={setMarked}
+        nodes={nodes}
+        setNodes={setNodes}
+        onNodesChanges={onNodesChange}
+        setEdges={setEdges}
+      /> : renderPage === 'profile' ? <div>PROFILE PLACEHOLDER</div>
+        : <DashboardMode renderPage={renderPage} setRenderPage={setRenderPage} />}
     </div>
     
   )

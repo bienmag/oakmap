c669b5939893a0cad7b173a025a22fbbfe8c7478
Manel Soler <manelsoler@gmail.com>
2023-02-14 22:07:11 +0100

    refactor: fix typescript types on all files

--- apps/web/Components/Modes/ModeSelector.tsx
+++ apps/web/Components/Modes/ModeSelector.tsx
@@ -8,24 +8,24 @@ import { NodesContextProvider } from '../../Resources/Packages/RFlow/NodesContex
 
 // dashboardMode: any,treeEditorMode: any
 function ModeSelector({ treeMode,
-setTreeMode,
-marked,
-setMarked,
-renderPage,
-setRenderPage,
-currentTreeId,
-setCurrentTreeId,
+  setTreeMode,
+  marked,
+  setMarked,
+  renderPage,
+  setRenderPage,
+  currentTreeId,
+  setCurrentTreeId,
 }: any) {
 
 
-return(
-  <div> {/* Render dashboard by default */ }
-    {/* renderPage === 'tree' ?
+  return (
+    <div> {/* Render dashboard by default */}
+      {/* renderPage === 'tree' ?
      : renderPage === 'profile' ? <div>PROFILE PLACEHOLDER</div>
       : <DashboardMode renderPage={renderPage} setRenderPage={setRenderPage} /> */}
-  </div>
-  
-)
+    </div>
+
+  )
 
 }
 

0c47fc6050a8d219ed289589a6cffef417015736
Josh <87762857+radna0@users.noreply.github.com>
2023-02-15 11:34:17 +0700

    Fix Main Description Disappearing

--- apps/web/Components/DescriptionMenu/DescriptionMenu.tsx
+++ apps/web/Components/DescriptionMenu/DescriptionMenu.tsx
@@ -1,18 +1,18 @@
-
 // Draggable bottom sheet for description
 import dynamic from 'next/dynamic'
+import { useState } from 'react'
 const Draggable = dynamic(() => import('react-draggable-bottom-sheet'), {
-  ssr: false
+  ssr: false,
 })
 import ReactMarkdown from 'react-markdown'
 
-function DescriptionMenu({ isOpen, closeBottomSheet, setDescription, description, treeMode }: any) {
-
+function DescriptionMenu({ isOpen, closeBottomSheet, treeMode }: any) {
+  const [description, setDescription] = useState<string>('')
   return (
     <Draggable isOpen={isOpen} close={closeBottomSheet}>
-      <div style={{ textAlign: "center", padding: "0px 16px 16px 16px" }}>
-        {treeMode === 'editor' ?
-          (<div>
+      <div style={{ textAlign: 'center', padding: '0px 16px 16px 16px' }}>
+        {treeMode === 'editor' ? (
+          <div>
             <h3>Description</h3>
             <textarea
               className="description-area"
@@ -20,15 +20,16 @@ function DescriptionMenu({ isOpen, closeBottomSheet, setDescription, description
               value={description}
               style={{ minHeight: '100px', minWidth: '1100px' }}
             ></textarea>
-          </div>)
-          : (<div>
+          </div>
+        ) : (
+          <div>
             <h3>Description</h3>
             <ReactMarkdown>{description}</ReactMarkdown>
-          </div>)}
+          </div>
+        )}
       </div>
     </Draggable>
   )
-
 }
 
 export default DescriptionMenu
